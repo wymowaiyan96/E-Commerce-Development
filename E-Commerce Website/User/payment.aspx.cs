32c34c67c3f7baf9c52b01b8e02a90750da6aa36
wymow <wymowaiyan96@gmail.com>
2019-01-10 18:27:40 +0800

    Paypal Error Fdexi
    the bug in paypal has been fixed

--- E-Commerce Website/User/payment.aspx.cs
+++ E-Commerce Website/User/payment.aspx.cs
@@ -38,37 +38,36 @@ public partial class User_payment : System.Web.UI.Page
                     a[j] = strArr1[j].ToString();
                 }
                 total = total + (Convert.ToInt32(a[2].ToString()) * Convert.ToInt32(a[3].ToString()));
-                Session["total"] = total.ToString(); //the total amount will be passsed to checkout
-
-                //Paypal 
-
-                order_no = Class1.GetRandomPassword(10).ToString();
-                Session["order_no"] = order_no.ToString();
-
-                Response.Write("<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method ='post' name='buyCredits' id='buyCredits'>");
-                Response.Write("<input type='hidden' name='cmd' value='_xclick'>");
-                //receive money address (owner)
-                Response.Write("<input type='hidden' name='business' value='wymowaiyan96@gmail.com'>"); 
-                //currency
-                Response.Write("<input type='hidden' name='currency_code' value='USD'>"); 
-                //description 
-                Response.Write("<input type='hidden' name='item_name' value='payment for purchase'>");                                                                                                       
-                //item Number                                                                                                                                                                      //item Number
-                Response.Write("<input type='hidden' name='item_number' value='0'>");
-                //amount to pay
-                Response.Write("<input type='hidden' name='amount' value='"+ Session["total"].ToString()+"'>");
-                //return after completing payment <VIEW CODE>
-                //this order no value will be passed
-                Response.Write("<input type='hidden' name='return' value='http://localhost:59404/User/payment_success.aspx?order=" + order_no.ToString() +"'>"); 
-                Response.Write("</form>");
-
-                Response.Write("<script type='text/javascript'>");
-                Response.Write("document.getElementById('buyCredits').submit();");
-                Response.Write("</script>");
-
-
-
+                           
             }
+            Session["total"] = total.ToString(); //the total amount will be passsed to checkout
+
         }
+        //Paypal 
+
+        order_no = Class1.GetRandomPassword(10).ToString();
+        Session["order_no"] = order_no.ToString();
+
+        Response.Write("<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method ='post' name='buyCredits' id='buyCredits'>");
+        Response.Write("<input type='hidden' name='cmd' value='_xclick'>");
+        //receive money address (owner)
+        Response.Write("<input type='hidden' name='business' value='wymowaiyan96@gmail.com'>");
+        //currency
+        Response.Write("<input type='hidden' name='currency_code' value='USD'>");
+        //description 
+        Response.Write("<input type='hidden' name='item_name' value='payment for purchase'>");
+        //item Number                                                                                                                                                                      //item Number
+        Response.Write("<input type='hidden' name='item_number' value='0'>");
+        //amount to pay
+        Response.Write("<input type='hidden' name='amount' value='" + Session["total"].ToString() + "'>");
+        //return after completing payment <VIEW CODE>
+        //this order no value will be passed
+        Response.Write("<input type='hidden' name='return' value='http://localhost:59404/User/payment_success.aspx?order=" + order_no.ToString() + "'>");
+        Response.Write("</form>");
+
+        Response.Write("<script type='text/javascript'>");
+        Response.Write("document.getElementById('buyCredits').submit();");
+        Response.Write("</script>");
+
     }
 }
